e70ccf4546cffc5991ba975dab8a3b3c45663263
maxis7567 <mahdi.sy1994@outlook.com>
2019-04-11 16:18:59 +0430

    DisplayMetrics FaNum NetworkChangeReceiver SoftKeyBoardManager StringContains

--- mstools/src/main/java/com/hinext/maxis7567/mstools/NetworkCheck.java
+++ mstools/src/main/java/com/hinext/maxis7567/mstools/NetworkCheck.java
@@ -5,16 +5,17 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import androidx.core.content.ContextCompat;
+
 
 public class NetworkCheck {
 
-        private Boolean isNetworkAvailable(Context context) {
+        public static boolean isNetworkAvailable(Context context) {
             ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
             return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
         }
-    public Boolean isOnline() {
+    public static boolean isOnline() {
         try {
             Process p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
             int returnVal = p1.waitFor();
--- mstools/src/main/java/com/hinext/maxis7567/mstools/PriceConvertor.java
+++ mstools/src/main/java/com/hinext/maxis7567/mstools/PriceConvertor.java
@@ -2,9 +2,13 @@ package com.hinext.maxis7567.mstools;
 
 import java.text.DecimalFormat;
 
+import static com.hinext.maxis7567.mstools.FaNumToEnNum.convertTofa;
+
+
 public class PriceConvertor {
     public static String Convert(long price){
         DecimalFormat decimalFormat = new DecimalFormat("#,##0");
-        return decimalFormat.format(price);
+        return (decimalFormat.format(price));
     }
+
 }
